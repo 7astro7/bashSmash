1490e064c0a9f408394aeb6c0e05ccbe1a86841c
Zak <zaknyy@protonmail.com>
2020-07-26 06:55:25 -0400

    add documentation to tiny script and remove a misplaced xlsx file

--- stackData.sh
+++ stackData.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Line below can be changed to explictly ask what name the
+# user wants prefixed to .csv for output file
 echo Please enter the year these CSVs pertain to
 
 read yearvar
 
 echo You entered $yearvar, output csv will be: ${yearvar}.csv
 
-csvstack * > ${yearvar}.csv
+# Binds rows of all csv files in cwd, generates new csv file 
+# with user input prefixed before the .csv. If there's already 
+# a csv file with such a name the output of row binding will 
+# be appended to the existing file
+csvstack * >> ${yearvar}.csv
